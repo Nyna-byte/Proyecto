17bfb6ddca2692eab98087670ef16c7320b3f874
SantiagoSanguino <91522514+SantiagoSanguino@users.noreply.github.com>
2022-05-29 09:55:39 +0200

    MVC
    Cambio el modelo de calculo a mvc

---
+++ vista/vista-calculo.php
@@ -0,0 +1,15 @@
+<?php
+if($TCtotal!==0){
+    $operarios=calcOperarios($cantidad,$TCtotal,$tiempo); //El tc entre 100 porque son centésimas de minuto
+    $equilibrado=calcEquilibrado($TCtotal,$operarios);
+    ?> 	<p>Número de operiarios: <?php echo $operarios; ?></p>
+		<br/>
+		<p>Base de equilibrado: <?php echo $equilibrado; ?></p>
+<?php
+}
+else{ ?>
+		<p>No se han introducido tiempos</p>
+<?php
+}
+
+?>
