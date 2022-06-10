a10c2fadf61df04b94cac6be59b4c1de5707e23c
SantiagoSanguino <91522514+SantiagoSanguino@users.noreply.github.com>
2022-06-10 10:38:25 +0200

    Update crearbbddprendas.sql

--- crearbbddprendas.sql
+++ crearbbddprendas.sql
@@ -1,11 +1,2 @@
 drop database prendas;
 create database prendas;
-use prendas;
-
-create table camisa (n_fase varchar(3), descripcion_fase varchar(20), maquina varchar(20), puntada varchar(20), rpm varchar(10), ppc varchar(10), tc varchar(10), observaciones varchar(80)) ENGINE=InnoDB;
-/** create table $nombre_prenda */
-alter table camisa add constraint pk_n_fase primary key (n_fase);
-
-insert into camisa (n_fase,descripcion_fase,maquina,tc) values (1,Orillar tela y entretela de tira ,plana,120);
-
-
